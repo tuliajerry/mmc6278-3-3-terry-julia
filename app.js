0d53a3422b1c7ecc0465e74fb8ed7125c4d74271
Julia Terry <156032724+tuliajerry@users.noreply.github.com>
2024-09-12 17:14:37 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,26 +1,21 @@
 require('dotenv').config();
 const express = require('express');
 const path = require('path');
-const { getCityInfo, getJobs } = require('./util'); 
+const { getCityInfo, getJobs } = require('./util');
 const app = express();
 
-
 app.use(express.static(path.join(__dirname, 'public')));
-
 
 app.get('/api/city/:city', async (req, res) => {
     const city = req.params.city;
 
     try {
-    
         const cityInfo = await getCityInfo(city);
         const jobs = await getJobs(city);
 
         if (cityInfo && jobs) {
-     
             res.json({ cityInfo, jobs });
         } else {
-
             res.status(404).json({ message: 'City data not found' });
         }
     } catch (error) {
@@ -30,3 +25,4 @@ app.get('/api/city/:city', async (req, res) => {
 });
 
 module.exports = app;
+
